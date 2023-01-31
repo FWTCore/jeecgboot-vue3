28f4fcd5f092007f18ddb61f70a09f6287507921
xuchase <121086507@qq.com>
2023-01-31 12:31:49 +0800

    修改标题名称

--- src/locales/lang/en/sys.ts
+++ src/locales/lang/en/sys.ts
@@ -69,7 +69,7 @@ export default {
     signUpFormTitle: 'Sign up',
     forgetFormTitle: 'Reset password',
 
-    signInTitle: 'Backstage management system',
+    signInTitle: 'MZX Management System',
     signInDesc: 'Enter your personal details and get started!',
     policy: 'I agree to the xxx Privacy Policy',
     scanSign: `scanning the code to complete the login`,
